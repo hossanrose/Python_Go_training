f47da27e7fe93691b184ae3aa6a4212e5bf3260d
Hossan Rose <hossan@LAP.hossanrose.com>
2019-09-26 13:07:23 +0100

    comment

--- go_training/23_defer/defer.go
+++ go_training/23_defer/defer.go
@@ -2,10 +2,12 @@ package main
 import "fmt"
 
 func main(){
+  // defer will run the deferred function only after the surrounding functions
   defer fmt.Print("world")
   fmt.Print("Hello ")
 
   fmt.Println("counting")
+  //Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
   for i:=0;i<10;i++{
     defer fmt.Println(i)
   }
