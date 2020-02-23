4ea718bc04bd1461ca2a2e8d39fa315069eefee4
Hossan Rose <me@hossanrose.com>
2020-02-23 07:22:02 +0000

    Refresher

---
+++ flask_python/01_A_user_input.py
@@ -0,0 +1,9 @@
+
+##For strings
+name=input("Give me your name")
+print(name)
+
+## For nunber processing 
+
+number=input("Give me a number to multiply it with 10")
+print(10* int(number))
--- flask_python/01_variables.py
+++ flask_python/01_variables.py
@@ -4,3 +4,10 @@ def my_method (num1, num2):
     return num1 * num2
 
 print(my_method(2,3))
+
+
+## Stirng formatting
+
+name="Hossan"
+
+print(f"My name is {name}") # formatting requires "f" in the starting and variables inside "{}"
